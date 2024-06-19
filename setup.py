0774c7ec445541ef71edf0a90b253c7c35d1d801
“Andrew <xhinker@hotmail.com>
2024-06-19 15:00:44 -0700

    update project structure

--- setup.py
+++ setup.py
@@ -2,20 +2,27 @@ from setuptools import setup,find_packages
 
 setup(
     name               = 'sd_embed'
-    , version          = '1.240617.1'
+    , version          = '1.240618.1'
     , license          = 'Apache License'
     , author           = "Andrew Zhu"
     , author_email     = 'xhinker@hotmail.com'
-    , packages         = find_packages('src')
-    , package_dir      = {'': 'src'}
+    , packages         = find_packages('sd_embed/src')
+    , package_dir      = {'': 'sd_embed/src'}
     , url              = 'https://github.com/xhinker/sd_embed'
     , keywords         = 'diffusers stable-diffusion embedding'
     , install_requires = [
         'torch'
         , 'torchvision'
         , 'torchaudio'
+        , 'sentencepiece'
+        , 'accelerate'
+        , 'peft'
         , 'transformers'
         , 'diffusers'
+        , 'lark'
+        , 'protobuf'
+        , 'ipykernel'
+        , 'ipywidgets'
     ]
     , include_package_data=True
 )
